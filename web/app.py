2c9c12cb566a49591bc28f35fda431ef94cddfd5
Sarah George <sngeorge27@gmail.com>
2023-02-27 17:10:45 -0500

    moving index.js

--- web/app.py
+++ web/app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def index():
     return render_template("./index.html")
 
-@app.route("/login")
-def index():
-    return render_template("./login.html")
+# @app.route("/login")
+# def index():
+#     return render_template("./login.html")
 
 if __name__ == "__main__":
     app.run()
