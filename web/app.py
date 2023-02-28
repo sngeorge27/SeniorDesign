7908327408b71089e5da57f9001c4bf381eb4a91
Brandon Jones <jones2bw@mail.uc.edu>
2023-02-28 08:18:28 -0500

    added routing and base template
    added routing and appropriate styling for nav to show which page you're on and created a base html which other html files will extend on

--- web/app.py
+++ web/app.py
@@ -2,13 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def index():
-    return render_template("./index.html")
+    return render_template("./index.html", route="/")
 
-@app.route("/login")
-def index():
-    return render_template("./login.html")
+@app.route("/goals")
+def goals():
+    return render_template("./goals.html", route="/goals")
+
+@app.route("/progress")
+def progress():
+    return render_template("./progress.html", route="/progress")
+
+@app.route("/track")
+def track():
+    return render_template("./track.html", route="/track")
+
+@app.route("/profile")
+def profile():
+    return render_template("./profile.html", route="/profile")
+
+@app.route("/settings")
+def settings():
+    return render_template("./settings.html", route="/settings")
+
 
 if __name__ == "__main__":
     app.run()
